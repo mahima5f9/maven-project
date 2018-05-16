0ad9b1da96aaaa8e5416e131c568590376b94395
mahima5f9 <mahimathota@gmail.com>
2018-05-16 11:03:30 +0530

    package file

---
+++ Greeter.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  /* this is a comment*/
+
+  public final String greet(final String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
